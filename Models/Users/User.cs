14204f9d708d62344ee319740d15a33ac0c6173b
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-04-19 11:18:50 +0500

    Add project files.

---
+++ Models/Users/User.cs
@@ -0,0 +1,21 @@
+﻿//==============================================================
+//Nasrullayev Nodirbek's UserManagment project
+//==============================================================
+
+namespace UserManagement.Core.Models.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public UserStatus Status { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public DateTime? LastLoginTime { get; set; }
+        public string? EmailConfirmationToken { get; set; }
+        public DateTime? TokenExpiresAt { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Models/Users/UserStatus.cs
@@ -0,0 +1,13 @@
+﻿//==============================================================
+//Nasrullayev Nodirbek's UserManagment project
+//==============================================================
+
+namespace UserManagement.Core.Models.Users
+{
+    public enum UserStatus
+    {
+        Unverified = 0,
+        Active = 1,
+        Blocked = 2
+    }
+}
