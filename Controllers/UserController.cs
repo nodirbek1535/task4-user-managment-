4f33c6eab6dff47b2045cea34e38cb1de158a83d
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-04-19 22:24:24 +0500

    CONTROLLERS: Add PostUserAsync

---
+++ Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿//==============================================================
+//Nasrullayev Nodirbek's UserManagment project
+//==============================================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using task4_user_managment_.Models.Users.Exceptions;
+using task4_user_managment_.Services.Foundations.Users;
+using UserManagement.Core.Models.Users;
+
+namespace task4_user_managment_.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : RESTFulController
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService userService) =>
+            this.userService = userService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult> PostUserAsync(User user)
+        {
+            try
+            {
+                User addedUser =
+                    await this.userService.AddUserAsync(user);
+
+                return Created(addedUser);
+            }
+            catch (UserValidationException validationException)
+            {
+                return BadRequest(validationException.InnerException);
+            }
+            catch (UserDependencyValidationException dependencyValidationException)
+                when (dependencyValidationException.InnerException is AlreadyExistsUserException)
+            {
+                return Conflict(dependencyValidationException.InnerException);
+            }
+            catch (UserDependencyException dependencyException)
+            {
+                return InternalServerError(dependencyException.InnerException);
+            }
+            catch (UserServiceException serviceException)
+            {
+                return InternalServerError(serviceException.InnerException);
+            }
+        }
+    }
+}
