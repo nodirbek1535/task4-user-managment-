8f6bde9418e5e62422107225559b4487acb5a9ad
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-04-23 11:56:17 +0500

    BROKERS: Add SendEmailAsync

---
+++ Brokers/Emails/IEmailBroker.cs
@@ -0,0 +1,11 @@
+﻿//==============================================================
+//Nasrullayev Nodirbek's UserManagment project
+//==============================================================
+
+namespace task4_user_managment_.Brokers.Emails
+{
+    public interface IEmailBroker
+    {
+        ValueTask SendEmailAsync(string receiverAddress, string subject, string body);
+    }
+}
